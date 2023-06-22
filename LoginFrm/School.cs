8a6654124fd2a4f7f690e50cf8159eaed684ff13
Blithe_Zhu <49220637+AngryPigHi@users.noreply.github.com>
2023-06-22 17:50:29 +0800

    V3版本

---
+++ LoginFrm/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginFrm
+{
+    class School
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 学校地址
+        /// </summary>
+        public string DZ { get; set; }
+    }
+}
